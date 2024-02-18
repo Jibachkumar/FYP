299f095128392542bfe3205af0d67d8ec79a6eba
jibachh <subhamchaudhary199@gmail.com>
2024-02-19 01:31:50 +0545

    add auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      children: [
+        {
+          path: "/login",
+          element: <Login />,
+        },
+      ],
+    },
+  ]);
   return (
-    <>
-      <h1 className=" text-xl font-bold">Hello World</h1>
-    </>
+    <div>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </div>
   );
 }
 
